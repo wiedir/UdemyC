e39bbaaa75621c4f6c4c07acac98fd014920a273
wiedir <dirk.wieberneit@gmail.com>
2022-03-23 22:53:31 +0100

    WIP Chapter 8

---
+++ Chapter08_PointerArrays/PointerArray1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int length = 3;
+
+    int *array = (int *)malloc(length * sizeof(int));
+
+    for (unsigned int i = 0; i < length; i++)
+    {
+        array[i] = i;
+    }
+
+    for (unsigned int i = 0; i < length; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+
+    free(array);
+    array = NULL;
+
+    return 0;
+}
